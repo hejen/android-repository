485791ba1828b55565eab3f8024442a5b4b8fb65
heje <heje@heje-PC>
2013-07-12 21:47:50 +0800

    add number 860

--- KQhelper/src/com/kqhelper/MainActivity.java
+++ KQhelper/src/com/kqhelper/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends Activity {
 				qchw.execute("http://mfkp.qzapp.z.qq.com/qshow/cgi-bin/wl_card_mainpage?sid=AYASVZlbvJNzrPaLoUB6bKpb&g_f=19011");
 				QQCardHelperWorker qchw1 = new QQCardHelperWorker(findViewById(R.id.editText1));
 				qchw1.execute("http://mfkp.qzapp.z.qq.com/qshow/cgi-bin/wl_card_mainpage?sid=AdIgWDifiRcX8tASMSKYVb1Z&g_f=19011");
+				QQCardHelperWorker qchw2 = new QQCardHelperWorker(findViewById(R.id.editText1));
+				qchw2.execute("http://mfkp.qzapp.z.qq.com/qshow/cgi-bin/wl_card_mainpage?sid=ARgcN0nyTguSGHnYO2ZcJ0hc&g_f=19011");
 			}
 		});
 	}
