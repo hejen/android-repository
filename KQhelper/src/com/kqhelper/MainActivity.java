eb6e30dab1a3da6cf2f615cf1fe44476a912af23
heje <heje@heje-PC>
2013-07-16 23:37:34 +0800

    add listview

--- KQhelper/src/com/kqhelper/MainActivity.java
+++ KQhelper/src/com/kqhelper/MainActivity.java
@@ -1,5 +1,10 @@
 package com.kqhelper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -13,7 +18,8 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.kqhepler.R;
@@ -45,11 +51,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initListView();
+		initReceiver();
+		
 		Button btnStartWork = (Button)findViewById(R.id.startWork);
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.setPriority(1000);
-		intentFilter.addAction("com.kqhelper.message");
-		registerReceiver(new ServiceMessageReceiver(), intentFilter);
 		btnStartWork.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -60,6 +65,45 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
+
+	private void initReceiver() {
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.setPriority(1000);
+		intentFilter.addAction("com.kqhelper.message");
+		registerReceiver(new ServiceMessageReceiver(), intentFilter);
+	}
+
+
+	private void initListView() {
+		ListView workList = (ListView)findViewById(R.id.workList);
+		SimpleAdapter sa = new SimpleAdapter(this, getWorkList(), R.layout.vlist, new String[]{"cWorkType","cName","iStatus"}, new int[]{R.id.cWorkType,R.id.cName,R.id.iStatus});
+		workList.setAdapter(sa);
+	}
+
+
+	private List<? extends Map<String, ?>> getWorkList() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		 
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("cWorkType", "G1");
+        map.put("cName", "google 1");
+        map.put("iStatus", 1);
+        list.add(map);
+ 
+        map = new HashMap<String, Object>();
+        map.put("cWorkType", "G2");
+        map.put("cName", "google 2");
+        map.put("iStatus", 1);
+        list.add(map);
+ 
+        map = new HashMap<String, Object>();
+        map.put("cWorkType", "G3");
+        map.put("cName", "google 3");
+        map.put("iStatus", 1);
+        list.add(map);
+        return list;
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
